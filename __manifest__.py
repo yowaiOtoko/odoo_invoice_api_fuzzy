d57e3beca87428691e8eecbeb066977b5af13e28
yowaiotoko <bsg75g@gmail.com>
2026-03-27 14:55:23 -0400

    updated manifest

--- __manifest__.py
+++ __manifest__.py
@@ -3,6 +3,8 @@
     'version': '1.0.0',
     'category': 'Accounting',
     'summary': 'Create quotations and invoices via API with products by name or id, fuzzy matching',
+    'author': 'yowaiotoko',
+    'license': 'LGPL-3',
     'depends': [
         'account',
         'product',
